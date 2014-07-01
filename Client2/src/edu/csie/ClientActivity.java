5dc81f1c73d27f837cbc613dd5491734d3b501eb
legendary <legendary@legendary-B85M-D3H>
2014-07-01 17:23:52 +0800

    7/1

--- Client2/src/edu/csie/ClientActivity.java
+++ Client2/src/edu/csie/ClientActivity.java
@@ -1,12 +1,9 @@
 package edu.csie;
 
 import android.app.Activity;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.Handler;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,8 +11,8 @@ import android.widget.TextView;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.PhoneLookup;
 import android.database.Cursor;
+import android.content.DialogInterface;
 import android.content.ContentResolver;
-import android.content.Context;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -23,23 +20,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.Socket;
-import java.util.Enumeration;
 
 import com.example.client2.R;
 
 // 
 public class ClientActivity extends Activity {
 	public static Handler mHandler = new Handler();
-	TextView TextView01; // 用來顯示文字訊息
-	EditText EditText01; // 文字方塊
-	EditText EditText02; // 文字方塊
-	String tmp; // 暫存文字訊息
-	Socket clientSocket = null; // 客戶端socket
+	private TextView TextView01; // 用來顯示文字訊息
+	private EditText EditText02; // 文字方塊
+	private String msg; // 暫存文字訊息
+
+	private final static int STATE_SEND_ADDRESSBOOK = 0;
+	private final static int STATE_SEND_NAME = 1;
+
+	private int state = STATE_SEND_ADDRESSBOOK;
+
+	private Socket serverSocket; // 客戶端socket
+	private BufferedReader br;
+	private BufferedWriter bw;
 
 	private String getPhone() {
-		StringBuilder string = new  StringBuilder("");
+		StringBuilder string = new StringBuilder("");
 
 		// 得到ContentResolver对象
 		ContentResolver cr = getContentResolver();
@@ -67,8 +69,16 @@ public class ClientActivity extends Activity {
 				string.append(contact + ":" + PhoneNumber + "\n");
 			}
 		}
+
+		// 通訊錄結尾字串
+		string.append("2147483648\n");
+		/*
+		 * can't use TelephonyManager telManager = (TelephonyManager)
+		 * getSystemService(Context.TELEPHONY_SERVICE); String lineNumber =
+		 * telManager.getLine1Number(); string.append(lineNumber + "\n");
+		 */
 		cursor.close();
-		
+
 		return string.toString();
 	}
 
@@ -76,134 +86,144 @@ public class ClientActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Log.d("hello", "fq");
 
-		// 從資源檔裡取得位址後強制轉型成文字方塊
 		TextView01 = (TextView) findViewById(R.id.TextView01);
-		EditText01 = (EditText) findViewById(R.id.EditText01);
 		EditText02 = (EditText) findViewById(R.id.EditText02);
-		
-		
 
-		// 以新的執行緒來讀取資料
 		Thread t = new Thread(readData);
-
-		// 啟動執行緒
 		t.start();
 
-		// 從資源檔裡取得位址後強制轉型成按鈕
 		Button button1 = (Button) findViewById(R.id.Button01);
-
-		// 設定按鈕的事件
 		button1.setOnClickListener(new Button.OnClickListener() {
-			// 當按下按鈕的時候觸發以下的方法
 			public void onClick(View v) {
-				// 如果已連接則
-				if (clientSocket == null) {
-					TextView01.append("連線中fq");
-					return ;
-				}
-				if (clientSocket.isConnected()) {
+				if (serverSocket.isConnected()) {
+					if (state == STATE_SEND_NAME) {
+						try {
+							BufferedWriter bw;
+							bw = new BufferedWriter(new OutputStreamWriter(
+									serverSocket.getOutputStream()));
+							bw.write(EditText02.getText() + "\n");
+							bw.flush();
 
-					BufferedWriter bw;
-					if (EditText01.getText().toString() == ""
-							|| EditText02.getText().toString() == "") {
-						return;
+							TextView01.append(EditText02.getText() + "\n");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						// 送出欲查詢的人名後, 將文字方塊清空
+						EditText02.setText("");
 					}
-
-					try {
-						// 取得網路輸出串流
-						bw = new BufferedWriter(new OutputStreamWriter(
-								clientSocket.getOutputStream()));
-
-						// 寫入訊息
-						bw.write(EditText01.getText() + ":"
-								+ EditText02.getText() + "\n");
-
-						// 立即發送
-						bw.flush();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					// 將文字方塊清空
-					EditText02.setText("");
 				} else {
 					TextView01.append("not connected\n");
 				}
 			}
 		});
-
 	}
 
 	// 顯示更新訊息
 	private Runnable updateText = new Runnable() {
 		public void run() {
-			// 加入新訊息並換行
-			TextView01.append(tmp + "\n");
+			String[] s = msg.split("\t");
+			final int sourceId = Integer.parseInt(s[0]);
+			String name = s[1];
+			final String phoneNumber = s[2];
+
+			new AlertDialog.Builder(ClientActivity.this)
+					.setTitle("新訊息")
+					.setMessage(
+							"使用者" + sourceId + "向您要求: " + name
+									+ " 的電話號碼, 請問給予嗎?")
+					.setNegativeButton("No",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									try {
+										bw.write(sourceId + "\tNO\t" + "\n");
+										bw.flush();
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+								}
+							})
+					.setPositiveButton("Yes",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									try {
+										bw.write(sourceId + "\tYES\t" + phoneNumber + "\n");
+										bw.flush();
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+								}
+							}).show();
+		}
+	};
+
+	private Runnable displayPhoneNumber = new Runnable() {
+		@Override
+		public void run() {
+			// 電話號碼
+			TextView01.append(msg + "\n");
 		}
 	};
 	
-	private Runnable notConnected = new Runnable() {
+	private Runnable SendToServerThread = new Runnable() {
+		@Override
 		public void run() {
-			TextView01.append("!! not connected.\n");
+			// TODO Auto-generated method stub
+			try {
+				bw.write(msg + "\n");
+				bw.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	};
-
 	// 取得網路資料
 	private Runnable readData = new Runnable() {
+		@Override
 		public void run() {
-			// server端的IP
 			InetAddress serverIp;
 
 			try {
-				// 以內定(本機電腦端)IP為Server端
 				serverIp = InetAddress.getByName("140.123.230.32");
 				int serverPort = 5050;
-				clientSocket = new Socket(serverIp, serverPort);
+				serverSocket = new Socket(serverIp, serverPort);
+				br = new BufferedReader(new InputStreamReader(
+						serverSocket.getInputStream()));
+				bw = new BufferedWriter(new OutputStreamWriter(
+						serverSocket.getOutputStream()));
 
-				// 取得網路輸入串流
-				BufferedReader br = new BufferedReader(new InputStreamReader(
-						clientSocket.getInputStream()));
-				
-				int i = 0;
-
-				// 當連線後
-				while (clientSocket.isConnected()) {
-					if (i == 0) {
-
-						BufferedWriter bw;
-						
-						try {
-							// 取得網路輸出串流
-							bw = new BufferedWriter(new OutputStreamWriter(
-									clientSocket.getOutputStream()));
-
-							// 寫入訊息
-							bw.write(getPhone());
-
-							// 立即發送
-							bw.flush();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						
-						i = 1;
+				if (state == STATE_SEND_ADDRESSBOOK) {
+					try {
+						bw = new BufferedWriter(new OutputStreamWriter(
+								serverSocket.getOutputStream()));
+						bw.write(getPhone()); // warning: 最多寫入 8192 字元
+						bw.flush();
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
-					
-					// 取得網路訊息
-					tmp = br.readLine();
 
-					// 如果不是空訊息則
-					if (tmp != null)
-						// 顯示新的訊息
-						mHandler.post(updateText);
+					state = STATE_SEND_NAME;
 				}
-				mHandler.post(notConnected);
 
+				while ((msg = br.readLine()) != null) {
+					if (msg.matches("[\\d\\s]+")) {
+						mHandler.post(displayPhoneNumber);
+					} else if (msg.matches("(YES)|(NO)")) {
+						mHandler.post(SendToServerThread);
+					} else {
+						mHandler.post(updateText);
+					}
+				}
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	};
-
 }
