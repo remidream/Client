8699d2056da2f66d94b07fbad819f623a4849b64
Lengendary <legendary@legendary-B85M-D3H>
2014-08-05 17:24:04 +0800

    hi

--- Client2/src/edu/csie/ClientActivity.java
+++ Client2/src/edu/csie/ClientActivity.java
@@ -1,42 +1,57 @@
 package edu.csie;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.PhoneLookup;
 import android.database.Cursor;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import com.example.client2.R;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 
-// 
 public class ClientActivity extends Activity {
 	private Context mContext = this;
 	public static Handler mHandler = new Handler();
+	private NameListAdapter mAdapter;
 	private TextView TextView01; // 用來顯示文字訊息
 	private EditText EditText02; // 文字方塊
+	private ListView listView;
 	private String msg; // 暫存文字訊息
 
 	private final static int STATE_SEND_USER_INFO = 0;
@@ -54,6 +69,11 @@ public class ClientActivity extends Activity {
 
 	private String userName;
 	private String userPhone;
+	private String searchName;
+	private String showNameOrPhone;
+
+	private ArrayList<HashMap<String, String>> mTextList = new ArrayList<HashMap<String, String>>();
+	private ProgressDialog mProgressDialog;
 
 	private String getPhone() {
 		StringBuilder string = new StringBuilder("");
@@ -86,6 +106,7 @@ public class ClientActivity extends Activity {
 				getPhoneByName.put(contact, PhoneNumber);
 				string.append(contact + ":" + PhoneNumber + "\n");
 			}
+			phone.close();
 		}
 
 		// 通訊錄結尾字串
@@ -100,6 +121,8 @@ public class ClientActivity extends Activity {
 		return string.toString();
 	}
 
+	//private ServiceConnection test;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -107,42 +130,164 @@ public class ClientActivity extends Activity {
 
 		TextView01 = (TextView) findViewById(R.id.TextView01);
 		EditText02 = (EditText) findViewById(R.id.EditText02);
+		listView = (ListView) findViewById(R.id.listView);
 
-		userName = this.getIntent().getExtras().getString("name");
-		userPhone = this.getIntent().getExtras().getString("phone");
+		mAdapter = new NameListAdapter();
+		listView.setAdapter(mAdapter);
 
-		TextView01.append(userName + "\n");
-		TextView01.append(userPhone + "\n");
+		userName = getIntent().getExtras().getString("name");
+		userPhone = getIntent().getExtras().getString("phone");
+		
+		mProgressDialog = ProgressDialog.show(this, "登入中", "請稍後...");
 
-		Thread t = new Thread(readData);
-		t.start();
+		Thread t1 = new Thread(readData);
+		t1.start();
 
 		Button button1 = (Button) findViewById(R.id.Button01);
 		button1.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				if (serverSocket.isConnected()) {
-					if (state == STATE_WAIT) {
-						try {
-							BufferedWriter bw;
-							bw = new BufferedWriter(new OutputStreamWriter(
-									serverSocket.getOutputStream()));
-							bw.write(EditText02.getText() + "\n");
-							bw.flush();
+				if("".equals(EditText02.getText().toString())){
+					new AlertDialog.Builder(mContext)  
+                    .setMessage("請正確輸入姓名")  
+                    .setPositiveButton("Ok", null)  
+                    .show(); 
+					
+                    return;
+				}
+				if (state == STATE_WAIT) {					
+						new AlertDialog.Builder(ClientActivity.this)
+						.setTitle("確認視窗")
+						.setMessage(
+								"確定要送出查詢嗎？")
+						.setCancelable(false)
+						.setNegativeButton("No",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									return;
+								}
+							})
+					.setPositiveButton("Yes",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
 
-							TextView01.append(EditText02.getText() + "\n");
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+									try {
+										bw.write(EditText02.getText().toString() + "\n");
+										bw.flush();
+										searchName = EditText02.getText().toString();
+										EditText02.setText("");
+									} catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									
+								}
+							}).show();
+						
+					
+					
 
-						// 送出欲查詢的人名後, 將文字方塊清空
-						EditText02.setText("");
-					}
-				} else {
-					TextView01.append("not connected\n");
+					// 送出欲查詢的人名後, 將文字方塊清空
+					//searchName = EditText02.getText().toString();
+					//EditText02.setText("");
 				}
 			}
 		});
 	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			new AlertDialog.Builder(mContext)
+					.setCancelable(false)
+					.setTitle("Warning")
+					.setMessage("確定要離開嗎？")
+					.setPositiveButton("是",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									finish();
+								}
+							}).setNegativeButton("否", null).show();
+			return true;
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private Runnable testConnection = new Runnable() {
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				while (true) {
+					serverSocket.sendUrgentData(0xFF);
+					Thread.sleep(5000);
+				}
+			} catch (Exception ex) {
+				mHandler.post(showNetworkError);
+			}
+		}
+	};
+
+	private Runnable showNetworkError = new Runnable() {
+		@SuppressLint("NewApi")
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			if (ClientActivity.this.isFinishing()) {
+				return;
+			}
+			if (!ClientActivity.this.isDestroyed()) {
+				new AlertDialog.Builder(ClientActivity.this)
+						.setTitle("Error")
+						.setMessage("與伺服器中斷連線，請檢查是否連上網路。")
+						.setCancelable(false)
+						.setPositiveButton("確定",
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										finish();
+									}
+								}).show();
+			}
+		}
+	};
+
+	private Runnable error = new Runnable() {
+		public void run() {
+			new AlertDialog.Builder(ClientActivity.this)
+					.setTitle("登入失敗")
+					.setMessage("此手機號碼已有人使用，請檢查是否輸入正確。")
+					.setCancelable(false)
+					.setPositiveButton("確定",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									finish();
+									
+								}
+							}).show();
+		}
+	};
 
 	// 顯示更新訊息
 	private Runnable updateText = new Runnable() {
@@ -150,27 +295,32 @@ public class ClientActivity extends Activity {
 			String[] s = msg.split("\t");
 			final int sourceId = Integer.parseInt(s[0]);
 			String name = s[1];
-			String showNameOrPhone;
 			final String phoneNumber = s[2];
 			final String sourcePhone = s[3];
 			if (!getPhoneByName.containsKey(name)) {
 				return;
 			}
+
 			if (getNameByPhone.containsKey(sourcePhone)) {
 				showNameOrPhone = getNameByPhone.get(sourcePhone);
 			} else {
 				showNameOrPhone = sourcePhone;
 			}
-			NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+			NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 			Intent intent = new Intent(mContext, ClientActivity.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		     PendingIntent pendingIntent 
-		      = PendingIntent.getActivity(mContext, 0, intent, 0);		      
-			Notification notification = new Notification.Builder(mContext).setSmallIcon(R.drawable.ic_launcher)
-				     .setContentTitle("Client2")
-				     .setContentText("使用者" + showNameOrPhone + "向您要求: " + name
-								+ " 的電話號碼, 請問給予嗎?")
-				     .setContentIntent(pendingIntent).build();
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+					| Intent.FLAG_ACTIVITY_SINGLE_TOP);
+			PendingIntent pendingIntent = PendingIntent.getActivity(mContext,
+					0, intent, 0);
+			Notification notification = new Notification.Builder(mContext)
+					.setSmallIcon(R.drawable.ic_launcher)
+					.setContentTitle("Client2")
+					.setAutoCancel(true)
+					.setContentText(
+							"使用者" + showNameOrPhone + "向您要求: " + name
+									+ " 的電話號碼, 請問給予嗎?")
+					.setContentIntent(pendingIntent).build();
 			notificationManager.notify(R.drawable.ic_launcher, notification);
 
 			new AlertDialog.Builder(ClientActivity.this)
@@ -200,7 +350,8 @@ public class ClientActivity extends Activity {
 									try {
 										bw.write(sourceId + "\tYES\t"
 												+ phoneNumber + "\t"
-												+ sourcePhone + "\n");
+												+ sourcePhone + "\t"
+												+ userPhone + "\n");
 										bw.flush();
 									} catch (Exception e) {
 										e.printStackTrace();
@@ -210,13 +361,81 @@ public class ClientActivity extends Activity {
 		}
 	};
 
+	private Runnable register = new Runnable() {
+		private final static String MY_MESSAGE = "someone.sendmessage";
+
+		public void run() {
+			registerReceiver(mBroadcast, new IntentFilter(MY_MESSAGE));
+			Intent intent = new Intent();
+			intent.setAction(MY_MESSAGE);
+			sendBroadcast(intent);
+		}
+	};
+
 	private Runnable displayPhoneNumber = new Runnable() {
 		@Override
 		public void run() {
 			// 電話號碼
-			TextView01.append(msg + "\n");
+			Log.d("msg", msg);
+			String[] s = msg.split(":");
+			// s[0]: phone number
+			// s[2]: source phone number
+
+			HashMap<String, String> item = new HashMap<String, String>();
+			String sourceName;
+			if (getNameByPhone.containsKey(s[3])) {
+				sourceName = getNameByPhone.get(s[3]);
+			} else {
+				sourceName = s[3];
+			}
+			item.put("source name", sourceName);
+			item.put("goal name", searchName);
+			item.put("phone number", s[0]);
+
+			mTextList.add(item);
 		}
 	};
+
+	private class NameListAdapter extends BaseAdapter {
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return mTextList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			MessageListElement messageListElement = null;
+			if (convertView == null) {
+				convertView = LayoutInflater.from(mContext).inflate(
+						R.layout.adapter, null);
+				messageListElement = new MessageListElement(convertView);
+				convertView.setTag(messageListElement);
+			} else {
+				messageListElement = (MessageListElement) convertView.getTag();
+			}
+
+			String sourceName = mTextList.get(position).get("source name");
+			String goalName = mTextList.get(position).get("goal name");
+			String phoneNumber = mTextList.get(position).get("phone number");
+			messageListElement.setTextView(sourceName, goalName, phoneNumber);
+
+			return convertView;
+		}
+	}
 
 	// 取得網路資料
 	private Runnable readData = new Runnable() {
@@ -227,15 +446,22 @@ public class ClientActivity extends Activity {
 			try {
 				serverIp = InetAddress.getByName("140.123.230.32");
 				int serverPort = 5050;
-				serverSocket = new Socket(serverIp, serverPort);
+
+				//FriendlyFindApplication application = (FriendlyFindApplication) ((Activity) mContext)
+				//		.getApplication();
+				serverSocket = // application.getServerSocket();
+				new Socket(serverIp, serverPort);
+
+				Thread t2 = new Thread(testConnection);
+				t2.start();
+
 				br = new BufferedReader(new InputStreamReader(
 						serverSocket.getInputStream()));
 				bw = new BufferedWriter(new OutputStreamWriter(
 						serverSocket.getOutputStream()));
-
 				if (state == STATE_SEND_USER_INFO) {
-					bw = new BufferedWriter(new OutputStreamWriter(
-							serverSocket.getOutputStream()));
+				//	bw = new BufferedWriter(new OutputStreamWriter(
+				//		serverSocket.getOutputStream()));
 					bw.write(userName + "\t" + userPhone + "\n");
 					bw.write(getPhone()); // warning: 最多寫入 8192 字元
 					bw.flush();
@@ -243,17 +469,33 @@ public class ClientActivity extends Activity {
 					state = STATE_WAIT;
 				}
 
+				mProgressDialog.dismiss();
+				
 				while ((msg = br.readLine()) != null) {
-					if (msg.matches("[\\d\\s]+")) {
+					if (msg.matches("[\\d\\s]+(:123456789:)[\\d\\s]+:[\\d\\s]+")) {
 						mHandler.post(displayPhoneNumber);
+					} else if (msg.matches("Error: [\\w\\s]+")) {
+						mHandler.post(error);
 					} else {
-						mHandler.post(updateText);
+						mHandler.post(register);
+						// mHandler.post(updateText);
 					}
 				}
-
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+	};
+
+	private BroadcastReceiver mBroadcast = new BroadcastReceiver() {
+		private final static String MY_MESSAGE = "someone.sendmessage";
+
+		@Override
+		public void onReceive(Context context, Intent mintent) {
+			if (MY_MESSAGE.equals(mintent.getAction())) {
+				mHandler.post(updateText);
+			}
+			unregisterReceiver(mBroadcast);
 		}
 	};
 }
--- Client2/src/edu/csie/LoginActivity.java
+++ Client2/src/edu/csie/LoginActivity.java
@@ -1,45 +1,149 @@
 package edu.csie;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 import com.example.client2.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
-	private Context context;
+	private SharedPreferences mPreferences;
+	private Context mContext;
 	private EditText mNameEditText;
 	private EditText mPhoneEditText;
+	private CheckBox mCheckBox;
 	private Button mLoginButton;
+	int correct;
+	Bundle bundle = new Bundle();
+	private static Handler mHandler = new Handler();
+	private Runnable iferror = new Runnable() {
+
+		@Override
+		public void run() {
+			if (mPhoneEditText.getText().length() != 10) {
+				// TODO Auto-generated method stub
+				Toast.makeText(mContext, "請輸入正確的手機號碼！", Toast.LENGTH_SHORT)
+						.show();
+				mPhoneEditText.setText("");
+				correct = 0;
+			}
+			if (correct == 1) {
+				Intent intent = new Intent();
+				intent.setClass(mContext, ClientActivity.class);
+				intent.putExtras(bundle);
+				startActivity(intent);
+			}
+		}
+	};
+
+	public boolean isOnline() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		context = this;
-		
-		mNameEditText = (EditText)findViewById(R.id.nameEditText);
-		mPhoneEditText = (EditText)findViewById(R.id.phoneEditText);
-		mLoginButton = (Button)findViewById(R.id.loginButton);
+		mContext = this;
+		mPreferences = getPreferences(MODE_PRIVATE);
+
+		mNameEditText = (EditText) findViewById(R.id.nameEditText);
+		mNameEditText.setText(mPreferences.getString("NAME", ""));
+		mPhoneEditText = (EditText) findViewById(R.id.phoneEditText);
+		mPhoneEditText.setText(mPreferences.getString("PHONE", ""));
+		mCheckBox = (CheckBox) findViewById(R.id.rememberCheckBox);
+		mCheckBox.setChecked(mPreferences.getBoolean("REMEMBER", false));
+		mLoginButton = (Button) findViewById(R.id.loginButton);
+
 		mLoginButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Bundle bundle = new Bundle();
-				bundle.putString("name", mNameEditText.getText().toString());
-				bundle.putString("phone", mPhoneEditText.getText().toString());
-				
-				Intent intent = new Intent();
-				intent.setClass(context, ClientActivity.class);
-				intent.putExtras(bundle);
-				startActivity(intent);
-			}			
+				correct = 1;
+				if (isOnline()) {
+					Thread thread = new Thread(new Runnable() {
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							Socket serverSocket = null;
+							try {
+								InetAddress serverIp = InetAddress
+										.getByName("140.123.230.32");
+								final int serverPort = 5050;
+
+								serverSocket = new Socket(serverIp, serverPort);
+								serverSocket.close();
+								//FriendlyFindApplication application = (FriendlyFindApplication) ((Activity) mContext)
+								//		.getApplication();
+								//application.setServerSocket(serverSocket);
+								
+								// Bundle bundle = new Bundle();
+								bundle.putString("name", mNameEditText
+										.getText().toString());
+								bundle.putString("phone", mPhoneEditText
+										.getText().toString());
+								
+								SharedPreferences.Editor editor = mPreferences.edit();
+								if (mCheckBox.isChecked()) {
+									editor.putString("NAME", mNameEditText.getText().toString());
+									editor.putString("PHONE", mPhoneEditText.getText().toString());
+									editor.putBoolean("REMEMBER", true);
+								} else {
+									editor.clear();
+								}
+								editor.commit();
+								
+								mHandler.post(iferror);
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+
+								if (serverSocket == null) {
+									mHandler.post(new Runnable() {
+										@Override
+										public void run() {
+											// TODO Auto-generated method stub
+											Toast.makeText(mContext, "網路異常",
+													Toast.LENGTH_SHORT).show();
+										}
+									});
+								}
+							}
+						}
+					});
+					thread.start();
+				} else {
+					AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+							mContext);
+					alertDialog.setTitle("Error");
+					alertDialog.setMessage("請檢查是否有連上網路。");
+					alertDialog.setPositiveButton("確定", null);
+					alertDialog.setCancelable(false);
+					alertDialog.show();
+				}
+			}
 		});
 	}
 }
